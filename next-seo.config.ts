685c6d96498bef4779527bc50cea6720de718824
Mostafa Darehzereshki <mostafa69d@gmail.com>
2024-08-23 18:20:09 -0400

    Updates SEO

--- next-seo.config.ts
+++ next-seo.config.ts
@@ -3,22 +3,17 @@ const SEO = {
     description: "Discover ADHD Therapy GPT, an AI-powered advisor designed to help you manage ADHD. Get personalized advice and strategies without replacing medical advice.",
     openGraph: {
       type: 'website',
-      url: 'https://www.yourdomain.com/',
+      url: 'https://adhdassistbot.com',
       title: 'ADHD Therapy GPT: AI-Powered ADHD Guidance & Support',
       description: 'Discover ADHD Therapy GPT, an AI-powered advisor designed to help you manage ADHD. Get personalized advice and strategies without replacing medical advice.',
       images: [
         {
-          url: 'https://www.yourdomain.com/images/og-image.jpg',
+          url: '/images/og-image.jpeg',
           width: 800,
           height: 600,
           alt: 'ADHD Therapy GPT',
         },
       ],
-    },
-    twitter: {
-      handle: '@yourhandle',
-      site: '@site',
-      cardType: 'summary_large_image',
     },
     additionalMetaTags: [
       {
@@ -27,7 +22,7 @@ const SEO = {
       },
       {
         name: 'author',
-        content: 'Your Name or Company',
+        content: 'Mostafa Darehzereshki',
       },
       {
         name: 'robots',
